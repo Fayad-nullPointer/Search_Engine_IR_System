745db6a9ed7c5a56b02ac8c2f8594fde653d706a
Fayad-nullPointer <fayadahmed037@gmail.com>
2024-05-08 02:49:48 +0300

    Modify at this file to use it at library

--- Analyise_Pathes.py
+++ Analyise_Pathes.py
@@ -34,16 +34,49 @@ def analyise_path(File_pathes):
     for i in range(len(array_MD)):
         dictionary2.setdefault(array_MD[i],list(set(dictionary[array_MD[i]])))
     return dictionary2
-def Anlyise_Mltiable_Path(File_number):
+def Anlyise_Mltiable_Path(x):
     ls=[]
-    for i in range(File_Number):
-        path=input("Enter Path number")
+    for i in range(x):
+        path=input("Enter Path")
         x=analyise_path(path)
         ls.append(x)
-    for i in range(File_Number):
-        print(ls[i])
-File_Number=int(input("Enter File Number"))
-Anlyise_Mltiable_Path(File_Number)
+    return ls
+   
+# File_Number=int(input("Enter File Number"))
+# listt=Anlyise_Mltiable_Path(File_Number)
+# print(listt)
+def analize_Query(query):
+     sentences=sent_tokenize(query)
+     tokenizer = RegexpTokenizer(r'\w+')
+     words = [tokenizer.tokenize(sentence) for sentence in sentences]
+     words = [[word.lower() for word in sentence] for sentence in words]
+     stemmer = PorterStemmer()
+     stemmed_words = [[stemmer.stem(word) for word in sentence] 
+                    for sentence in words]
+     array=np.array(stemmed_words)
+     array_MD=array.reshape(array.shape[1],)
+    #  print(array_MD)
+     dictionary={}
+     sum=0
+     for i in range(len(array_MD)):
+         dictionary.setdefault(array_MD[i],[])
+     for i in range(len(array_MD)):
+         x=array_MD[i]
+         for j in range(len(array_MD)-1):
+             if array_MD[i]==array_MD[j+1]:
+                sum=sum+1
+         dictionary[x].append(sum)
+         sum=0 
+     dictionary2={}
+     for i in range(len(array_MD)):
+         dictionary2.setdefault(array_MD[i],list(set(dictionary[array_MD[i]])))
+     return dictionary2
+
+
+# print(analize_Query("Hello world Hello World"))
+print(Anlyise_Mltiable_Path(2))
+print(analize_Query("Fayad NUMBER NUMBER FAYAD"))
+    
 
 
     
